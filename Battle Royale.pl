3d8dd955295a99bad3d3fe89e94bd58616d03ff6
vincent VB <42698353+vincentVB14@users.noreply.github.com>
2018-11-18 22:31:39 +0700

    Add files via upload

--- Battle Royale.pl
+++ Battle Royale.pl
@@ -5,35 +5,30 @@
 /* Tanggal			: 21 November 2018 */
 /* Deskripsi		: Membuat permainan battle royale menggunakan prolog */
 
-start. -- start the game!
-help. -- show available commands
-quit. -- quit the game
-look. -- look around you
-n. s. e. w. -- move
-map. -- look at the map and detect enemies
-take(Object). -- pick up an object
-drop(Object). -- drop an object
-use(Object). -- use an object
-attack. -- attack enemy that crosses your path
-status. -- show your status
-save(Filename). -- save your game
-load(Filename). -- load previously saved game
-
-Legends:
-W = weapon
-A = armor
-M = medicine
-O = ammo
-P = player
-E = enemy
-- = accessible
-X = inaccessible
-
-/* Deklarasi Fakta */
-
+/* Implementasi fungsi execute */
+execute(help) :- help, nl, !.
+execute(quit) :- quit, nl, !.
+execute(look) :- look, nl, !.
+execute(map) :- map, nl, !.
+execute(n) :- n, nl, !.
+execute(s) :- s, nl, !.
+execute(e) :- e, nl, !.
+execute(w) :- w, nl, !.
+execute(take(object)) :- take(object), nl, !.
+execute(drop(object)) :- drop(object), nl, !.
+execute(use(object)) :- use(object), nl, !.
+execute(attack) :- help, nl, !.
+execute(status) :- status, nl, !.
+/*execute(save(file)) :- save(file), nl, !.
+execute(load(file)) :- load(file), nl, !. */
 
 /* Deklarasi Rule */
-/* start() : memulai permainan */
+dynamic_facts :- retractall(currentlocation(X)), retractall(inventory(X)), assertz(currentlocation(1,1)), assertz(inventory(none)). 
+
+/* Loop agar game tetap berjalan */
+game() :- repeat, read(X), execute(X), (win; X==quit), !.
+
+/* start() : memulai permainan, menampilkan judul dan instruksi permainan */
 start :-
 write('  _____   _    _  ____    _____  _                                _       '), nl,
 write(' |  __ \\ | |  | ||  _ \\  / ____|| |                              | |      '), nl,
@@ -46,4 +41,70 @@ write('                                          |___/                          
 write(' Welcome to PUBG Legends! '), nl,
 write(' Five seconds before the enemy reaches the battlefield! Smash them! '), nl,
 sleep(1), write(' No troops deployed! '), nl,
-sleep(2), write(' You`re alone... ').
+sleep(2), write(' You`re alone... '), dynamic_facts, game().
+
+/* help() : menampilkan fungsi-fungsi yang dapat dipanggil dalam permainan, dapat mengandung informasi lain yang mungkin dibutuhkan */
+help :-
+write(' start -- start the game! '), nl,
+write(' help -- show available commands '), nl,
+write(' quit -- quit the game '), nl,
+write(' look -- look around you '), nl,
+write(' n -- move north '), nl,
+write(' s -- move south '), nl,
+write(' e -- move east '), nl,
+write(' w -- move west '), nl,
+write(' map -- look at the map and detect enemies '), nl,
+write(' take(Object). -- pick up an object '), nl,
+write(' drop(Object). -- drop an object '), nl,
+write(' use(Object). -- use an object '), nl,
+write(' attack. -- attack enemy that crosses your path '), nl,
+write(' status. -- show your status '), nl,
+write(' save(Filename). -- save your game '), nl,
+write(' load(Filename). -- load previously saved game '), nl, nl,
+write(' Legends: '), nl,
+write(' W = weapon '), nl,
+write(' A = armor  '), nl,
+write(' M = medicine  '), nl,
+write(' O = ammo  '), nl,
+write(' P = player  '), nl,
+write(' E = enemy  '), nl,
+write(' - = accessible  '), nl,
+write(' X = inaccessible ').
+
+/* quit() : mengakhiri permainan */
+quit :-
+write('  _______  _    _            _   _  _  __  __     __ ____   _    _  '), nl,
+write(' |__   __|| |  | |    /\\    | \\ | || |/ /  \\ \\   / // __ \\ | |  | | '), nl,
+write('    | |   | |__| |   /  \\   |  \\| || \' /    \\ \\_/ /| |  | || |  | | '), nl,
+write('    | |   |  __  |  / /\\ \\  | . ` ||  <      \\   / | |  | || |  | | '), nl,
+write('    | |   | |  | | / ____ \\ | |\\  || . \\      | |  | |__| || |__| | '), nl,
+write('    |_|   |_|  |_|/_/    \\_\\|_| \\_||_|\\_\\     |_|   \\____/  \\____/  '), nl,
+write('____________________________________________________________________'), nl,
+write('                                                                    '), nl,
+write('                               CREDIT                               '), nl,
+write('                                                                    '), nl,
+write('                 13517020 - T. Antra Oksidian Tafly                 '), nl,
+write('                      13517059 - Nixon Andhika                      '), nl,
+write('                    13517131 - Jan Meyer Saragih                    '), nl,
+write('                    13517137 - Vincent Budianto                     '), nl,
+sleep(2), halt.
+
+/* look() : menuliskan petak-petak 3x3 di sekitar pemain dengan posisi pemain saat ini menjadi center */
+
+
+/* n() : menggerakkan pemain satu petak ke arah utara  */
+
+
+/* s() : menggerakkan pemain satu petak ke arah selatan */
+
+
+/* e() : menggerakkan pemain satu petak ke arah timur */
+
+
+/* w() : menggerakkan pemain satu petak ke arah barat */
+
+
+/* map() : memperlihatkan seluruh peta permainan dengan menunjukkan petak deadzone dan petak safezone, serta lokasi pemaim */
+
+
+/* status() : menampilkan status pemain saat ini (health, armor, weapon, ammo) dan list barang yang ada di inventory */
