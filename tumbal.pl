33285fe9600118bf1b10c1c488d8449048f0bcc4
vincent VB <42698353+vincentVB14@users.noreply.github.com>
2018-11-19 14:37:18 +0700

    Update tumbal.pl

--- tumbal.pl
+++ tumbal.pl
@@ -9,3 +9,5 @@ drawMap(Row,Col,Timer) :-
   (Row >= 12, Col < 12, print('X '), !, NextCol is Col+1, map(Row,NextCol,Timer),!);
   (Row > 1, Col =< 1, print('X '), !, NextCol is Col+1, map(Row,NextCol,Timer), !);
   (Row > 1, Col >= 12, print('X'), nl, !, NextRow is Row+1, NextCol is 1, map(NextRow,NextCol,Timer),!).
+
+https://docs.google.com/document/d/1qMlWMTz2BXiIHx7sTOS_AHEqbOGl8tyZkwwc3B3W5VM/edit#
